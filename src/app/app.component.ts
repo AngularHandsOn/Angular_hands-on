07ba0dc3c05c19c022485391117199e281b5de4d
Ryosuke Yamano <ryosuke.y.97@gmail.com>
2017-06-13 10:25:25 +0900

    feat:AppComponentを分割

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,10 @@
 import { Component } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html' 
 })
+
 export class AppComponent {
-  title = 'Tour of Heroes';
-  heroes: Hero[];
-
-  constructor(private heroService: HeroService) {}
-
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
+    title = 'Tour of Heroes';
 }
