b3c74aeef296807964ae73afc16b48f323873553
Ryosuke Yamano <ryosuke.y.97@gmail.com>
2017-06-13 11:25:26 +0900

    feat:HeroesComponentからヒーローを選択できる

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
@@ -7,17 +9,13 @@ import { HeroService } from '../hero.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  title = 'Tour of Heroes';
+export class HeroesComponent implements OnInit {
   heroes: Hero[];
-
-  constructor(private heroService: HeroService) {}
-
   selectedHero: Hero;
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  constructor(
+    private router: Router,
+    private heroService: HeroService) {}
 
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
@@ -26,4 +24,13 @@ export class HeroesComponent {
   ngOnInit(): void {
     this.getHeroes();
   }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+  
 }
